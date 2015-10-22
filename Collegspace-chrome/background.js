2a18fbe33d3e0eff4fd6939b69ce347fa6043261
Sahil Dua <sahildua2305@gmail.com>
2015-10-23 01:49:05 +0530

    Comment and document code properly

--- Collegspace-chrome/background.js
+++ Collegspace-chrome/background.js
@@ -35,29 +35,39 @@ function getTimeDiff(date){
  * 		  It basically fetches the feed from wordpress website feed URL and shows according to their timestamp
  */
 function initialize(){
+	// set the feed to the wordpress website feed URL
 	var feed = new google.feeds.Feed("http://updates.collegespace.in/feed/");
+	// set the number of entries(updates/posts) to be fetched
 	feed.setNumEntries(10);
 	var count_today = 0;
+	// callback function called whenever the feed is loaded from the wordpress website feed URL
 	feed.load(function(result) {
-		if (!result.error) {
+		if (!result.error) { // if results are fetched successsfully, and no error has occured
+			// create 3 elements for today, last week and old posts sections
 			var today_posts = document.getElementById("today");
 			var last_week_posts = document.getElementById("last_week");
 			var old_posts = document.getElementById("old_posts");
+			
 			var html = "<ul>";
+			
+			// iterate over the result feed entries
 			for (var i = 0; i < result.feed.entries.length; i++) {
 				var entry = result.feed.entries[i];
 				html = "<li>" + "<a href='" + entry.link + "' target='_blank'>" + entry.title + "</a></li>";
+				// if time difference is less than one day
 				if(getTimeDiff(entry.publishedDate)<86400){
 					var div = document.createElement("div");
 					div.innerHTML = html;
 					today_posts.appendChild(div);
 					count_today++;
 				}
+				// if time difference is less than one week
 				else if(getTimeDiff(entry.publishedDate)<604800){
 					var div = document.createElement("div");
 					div.innerHTML = html;
 					last_week_posts.appendChild(div);
 				}
+				// otherwise, put in old posts
 				else{
 					var div = document.createElement("div");
 					div.innerHTML = html;
@@ -65,7 +75,11 @@ function initialize(){
 				}
 			}
 			html += "</ul>";
+			
+			// write the entire html to the document
 			document.write(html);
+			
+			// set the counter of the extension equal to the number of posts in `today` section
 			chrome.browserAction.setBadgeText({text:""+count_today});
 		}
 	});
